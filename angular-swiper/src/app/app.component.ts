0848c37bb186349ef6de15272f96731b4dfd12ba
Sooraj M <61335345+SOORAJ-SJ@users.noreply.github.com>
2022-06-09 12:17:46 +0000

    folder structure fix

--- angular-swiper/src/app/app.component.ts
+++ angular-swiper/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import SwiperCore ,{ SwiperOptions} from 'swiper';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-swiper';
+  config: SwiperOptions = {
+    lazy: true
+  }
 }
