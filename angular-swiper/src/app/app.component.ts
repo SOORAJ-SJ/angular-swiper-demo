c80a3f681ce243d5944d76171ce3745f5dfd5027
Sooraj M <61335345+SOORAJ-SJ@users.noreply.github.com>
2022-06-09 13:03:16 +0000

    added swiper images

--- angular-swiper/src/app/app.component.ts
+++ angular-swiper/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import SwiperCore ,{ SwiperOptions} from 'swiper';
 
 @Component({
@@ -6,9 +6,18 @@ import SwiperCore ,{ SwiperOptions} from 'swiper';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'angular-swiper';
   config: SwiperOptions = {
-    lazy: true
+    lazy: true,
+    preloadImages: false,
+  }
+  images: any[] =  [
+    'https://images.unsplash.com/photo-1649859398021-afbfe80e83b9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
+    'https://images.unsplash.com/photo-1654476728696-8344dfc78585?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
+    'https://images.unsplash.com/photo-1648737155328-0c0012cf2f20?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80'
+]
+  ngOnInit(): void {
+    console.log(this.images);
   }
 }
